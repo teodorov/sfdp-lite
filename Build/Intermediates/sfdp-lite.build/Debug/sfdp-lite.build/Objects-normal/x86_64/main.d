cb739e97a8dc8d2a1d3a580a70a06058f2eb3469
Ciprian TEODOROV <ciprian.teodorov@gmail.com>
2015-01-15 16:35:37 +0100

    fixed help issue

---
+++ Build/Intermediates/sfdp-lite.build/Debug/sfdp-lite.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,12 @@
+dependencies: \
+  /Users/ciprian/Playfield/repositories/sfdp-lite/sfdp-lite/main.c \
+  /Users/ciprian/Playfield/repositories/sfdp-lite/sfdp-lite/lib/mmio/mmio.h \
+  /Users/ciprian/Playfield/graphviz/graphviz-install/include/graphviz/SparseMatrix.h \
+  /Users/ciprian/Playfield/graphviz/graphviz-install/include/graphviz/general.h \
+  /Users/ciprian/Playfield/graphviz/graphviz-install/include/graphviz/spring_electrical.h \
+  /Users/ciprian/Playfield/cairo/cairo-1.12.14_install/include/cairo/cairo.h \
+  /Users/ciprian/Playfield/cairo/cairo-1.12.14_install/include/cairo/cairo-version.h \
+  /Users/ciprian/Playfield/cairo/cairo-1.12.14_install/include/cairo/cairo-features.h \
+  /Users/ciprian/Playfield/cairo/cairo-1.12.14_install/include/cairo/cairo-deprecated.h \
+  /Users/ciprian/Playfield/cairo/cairo-1.12.14_install/include/cairo/cairo-pdf.h \
+  /Users/ciprian/Playfield/repositories/sfdp-lite/sfdp-lite/jet.h
---
+++ Build/Intermediates/sfdp-lite.build/Debug/sfdp-lite.build/Objects-normal/x86_64/mmio.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/ciprian/Playfield/repositories/sfdp-lite/sfdp-lite/lib/mmio/mmio.c \
+  /Users/ciprian/Playfield/repositories/sfdp-lite/sfdp-lite/lib/mmio/mmio.h
